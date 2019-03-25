feaafc1de7011750399d5994e74ed1001a3c5c08
shanghai-Jerry <cjyou20110920sspu@163.com>
2019-03-25 16:03:38 +0800

    finished sample in tensorflow

--- tensor/api_test.py
+++ tensor/api_test.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import numpy as np
 
-w = tf.constant([[1,-0.5],[0.6,-0.4]], dtype=tf.float32)
+
+x = tf.constant([[1., 1.], [2., 2.]])
+
 with tf.Session() as sess:
-    print ("w:", sess.run(w))
-    print ("r:", sess.run(tf.nn.relu(w)))
-    print ("out:", sess.run(tf.nn.dropout(w, keep_prob=0.5)))
+    print ("result:", sess.run([tf.reduce_mean(x), tf.reduce_mean(x, 0, keepdims=True),tf.reduce_mean(x, 1, keepdims=True)]))
