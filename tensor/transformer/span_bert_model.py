d3ec47436a3500cf5d14ec61affec84ea42a74b5
shanghai-Jerry <cjyou20110920sspu@163.com>
2019-08-12 18:33:19 +0800

    finished span bert

--- tensor/transformer/span_bert_model.py
+++ tensor/transformer/span_bert_model.py
@@ -5,14 +5,14 @@
 
 # Copyright 2020 - 2019
 
+from collections import defaultdict
+
 import tensorflow as tf
 from tensorflow.python.ops import gen_math_ops
-import numpy as np
-import math
+
+import model_utils
 import optimization
 from transformer import Transformer, LayerNormalization
-from collections import defaultdict
-import model_utils
 
 kMaxSeqLen = 200
 kMaxLabel = 26
@@ -30,7 +30,7 @@ BASE_PARAMS = defaultdict(
     initializer_gain=2.0,  # Used in trainable variable initialization.
     initializer_range = 0.02,
     vocab_size=32003,  # Number of tokens defined in the vocabulary file.
-    hidden_size=200,  # Model dimension in the hidden layers.
+    hidden_size=200,  # Model dimension in the hidden layers, equal to embedding_size
     num_hidden_layers=6,  # Number of layers in the encoder and decoder stacks.
     num_heads=8,  # Number of heads to use in multi-headed attention.
     filter_size=800,  # Inner layer dimension in the feedforward network.
@@ -164,8 +164,7 @@ def parse_tfrecord_function(example_proto):
 class Model(object):
     def __init__(self,
                  embeddingSize,
-                 vocabSize=kVocabSize,
-                 lastHiddenSize=200):
+                 vocabSize=kVocabSize):
         self.max_token_per_sentence = kMaxSeqLen-1
         self.max_labels = kMaxLabel
         self.sentence_placeholder = tf.placeholder(
@@ -180,16 +179,22 @@ class Model(object):
             tf.int32, shape=[None, self.max_labels])
         self.span_right = tf.placeholder(
             tf.int32, shape=[None, self.max_labels])
+        # per_sentence 有效token的长度
         self.seqLength = tf.placeholder(
             tf.int32, shape=[None], name="length")
         
-        self.embedding_size = embeddingSize 
+        self.embedding_size = embeddingSize
+        self.vocab_size = vocabSize
+        # we can get embeddingSize,vocabSize from BASE_PARAMS["hidden_size"], BASE_PARAMS["vocab_size"],
+        # cause it's the same
+        # self.vocab_size =  BASE_PARAMS["vocab_size"]
+        # self.embedding_size = BASE_PARAMS["hidden_size"]
         self.transformer_conf=BASE_PARAMS
-        self.last_hidden_size = lastHiddenSize
+        # self.last_hidden_size = lastHiddenSize
         self.learning_rate_h = tf.placeholder(tf.float32, shape=[])
         self.dropout_h = tf.placeholder(tf.float32, shape=[], name="dropout")
-        self.vocab_size = vocabSize
         self.layer_norm = LayerNormalization(self.embedding_size)
+        # mask左右位置 + mask的位置向量预测mask词
         self.layer_norm_sbo = LayerNormalization(self.embedding_size)
         with tf.variable_scope("lm") as scope:
             # self.languge_model_weights = tf.get_variable(
@@ -216,7 +221,9 @@ class Model(object):
         with tf.variable_scope("tf_inference"):
             X = self.sentence_placeholder
             shapeS = tf.shape(X)
+            # cls 的初始化, id为整个词典走中最大的
             padCLS = tf.ones([shapeS[0], 1], dtype=tf.int32) * (kVocabSize-1)
+            # [cls] token1 token2 tokenN,,,,token199 = total length equal 200 include [cls]
             paddedX = tf.concat([padCLS, X], axis=1)
             _, outputs, posEncoding = transformer(paddedX, None)
             posEncoding = tf.tile([posEncoding], [shapeS[0],1,1])
@@ -225,11 +232,48 @@ class Model(object):
     def train(self, loss, lr, totalSteps, warnSteps):
         return optimization.create_optimizer(
             loss, lr, totalSteps, warnSteps, False)
+        
+    def loss(self, training=True):
+        transformer = Transformer(self.transformer_conf, training)
+        X, P = self.inference(transformer, name="inference_final" if not training else None, training=training)
+        labelLen = self.length(self.label_index)
+        shapeS = tf.shape(self.label_index)
+
+        todoX = batch_gather(X, self.label_index)
+        # 这个weight一直共享着 [self.vocab_size, self.embedding_size]
+        embeddings = transformer.embedding_softmax_layer.shared_weights
+        # 计算mlm具体loss
+        mlmAcc, mlmLoss , mlmSamecount= self.calc_acc_loss(embeddings, todoX, labelLen)
+
+        # 索引出left, right, 和mask对应位置需要的向量表示，mask位置取位置向量
+        todoSpanLeft = batch_gather(X, self.span_left)
+        todoSpanRight= batch_gather(X, self.span_right)
+        todoPosition = batch_gather(P, self.label_index)
+        print("P=%r, todoPosition=%r"%(P, todoPosition))
+        # concat : 3 * self.embedding_size
+        sboX = tf.concat([todoSpanLeft, todoSpanRight, todoPosition], axis=2)
+        sboX = tf.nn.xw_plus_b(tf.reshape(
+            sboX, [-1, self.embedding_size*3]), self.sbo_weights, self.sbo_bias)
+        sboX = tf.nn.relu(sboX)
+        if training:
+            sboX = tf.nn.dropout(sboX, 1.0-self.transformer_conf["relu_dropout"])
+        sboX = self.layer_norm_sbo(sboX)
+        # 计算sbo具体loss
+        sboAcc, sboLoss , sboSamecount= self.calc_acc_loss(embeddings, sboX, labelLen)
+       
+        return mlmLoss+sboLoss, mlmSamecount,sboSamecount, mlmAcc, sboAcc
 
     def calc_acc_loss(self, embeddings, X, labelLen):
+        # X:[batch_size, max_labels, embedding_size]
         logits = tf.nn.xw_plus_b(tf.reshape(
             X, [-1, self.embedding_size]), tf.transpose(embeddings), self.languge_model_bias)
-        mlmCost = model_utils.soft_cross_entropy_loss(logits,tf.reshape(self.label_target, [-1]),self.transformer_conf['label_smoothing'], self.transformer_conf['vocab_size'])
+
+        # MLM loss
+        mlmCost = model_utils.soft_cross_entropy_loss(logits,tf.reshape(self.label_target, [-1]),
+                                                      self.transformer_conf['label_smoothing'],
+                                                      self.transformer_conf['vocab_size'])
+
+        #
         preds = tf.reshape(tf.arg_max(logits, dimension=1,
                                       output_type=tf.int32), [-1, self.max_labels])
         sameCounts = tf.to_float(tf.equal(preds, self.label_target))
@@ -245,30 +289,3 @@ class Model(object):
         mlmCost = mlmCost / tf.cast(labelLen, tf.float32)
         mlmCost = tf.reduce_mean(mlmCost)
         return accuracy, mlmCost,sameCounts
-        
-    def loss(self, training=True):
-        transformer = Transformer(self.transformer_conf, training)
-        X, P = self.inference(transformer, name="inference_final" if not training else None, training=training)
-        labelLen = self.length(self.label_index)
-        shapeS = tf.shape(self.label_index)
-
-        todoX = batch_gather(X, self.label_index)
-
-        embeddings = transformer.embedding_softmax_layer.shared_weights
-        
-        mlmAcc, mlmLoss , mlmSamecount= self.calc_acc_loss(embeddings, todoX, labelLen)
-        
-        todoSpanLeft = batch_gather(X, self.span_left)
-        todoSpanRight= batch_gather(X, self.span_right)
-        todoPosition = batch_gather(P, self.label_index)
-        print("P=%r, todoPosition=%r"%(P, todoPosition))
-        sboX = tf.concat([todoSpanLeft, todoSpanRight, todoPosition], axis=2)
-        sboX = tf.nn.xw_plus_b(tf.reshape(
-            sboX, [-1, self.embedding_size*3]), self.sbo_weights, self.sbo_bias)
-        sboX = tf.nn.relu(sboX)
-        if training:
-            sboX = tf.nn.dropout(sboX, 1.0-self.transformer_conf["relu_dropout"])
-        sboX = self.layer_norm_sbo(sboX)
-        sboAcc, sboLoss , sboSamecount= self.calc_acc_loss(embeddings, sboX, labelLen)
-       
-        return mlmLoss+sboLoss, mlmSamecount,sboSamecount, mlmAcc, sboAcc
